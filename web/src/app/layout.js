23173ff3d7312eab6f1b70cf9f09ba9d7bc385db
Nouman-Shahid1 <muhammadnoumansha140@gmail.com>
2024-07-03 20:14:26 +0500

    updated

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -1,22 +1,22 @@
 "use client";
 import { Bona_Nova } from "@next/font/google";
-import dynamic from "next/dynamic";
-import store from "../store/store";
-import Head from "next/head";
+// import dynamic from "next/dynamic";
+// import store from "../store/store";
+// import Head from "next/head";
 import "./globals.css";
 
 const bonaNova = Bona_Nova({ weight: "400", subsets: ["latin"] });
 
-const DynamicProvider = dynamic(
-  () => import("react-redux").then((mod) => mod.Provider),
-  { ssr: true }
-);
+// const DynamicProvider = dynamic(
+//   () => import("react-redux").then((mod) => mod.Provider),
+//   { ssr: true }
+// );
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={bonaNova.className}>
-        <DynamicProvider store={store}>{children}</DynamicProvider>
+       {children}
       </body>
     </html>
   );
